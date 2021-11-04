d772966de7ff65db1694090455cbb3bf4b003bcd
Shane S <59064868+Shane-S20@users.noreply.github.com>
2021-11-04 19:01:16 -0400

    Add files via upload
    Version from 11/4 which has working getURL(), checking if URL is https, and basic login system

--- function.js
+++ function.js
@@ -1,30 +1,53 @@
 
-console.log("FUNCTION TEST");
+console.log("FUNCTION TEST")
 
-let btn = document.getElementById("Test_button");
+//let btn = document.getElementById("Test_button");
+//btn.addEventListener("click", test_URL)
 
-btn.addEventListener("click", test_URL)
+//let createUser = document.getElementById("create");
+//createUser.addEventListener("click",create_user);
 
-function test_URL()
+//function test_URL()
+$("#Test_button").on("click",function()
 {
-    //console.log("TEST");
-    //document.getElementById("result").innerHTML = "The full URL of this page is:<br>" + window.location.href;
-    var url = document.getElementById("URL_Box");
-    if (url.value.charAt(4) == "s")
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},function(tabs)
     {
-        document.getElementById("result").innerHTML = "URL is using HTTPS protocol";
-        //alert("URL is using HTTPS");
-    }
-    else 
-    {
-        document.getElementById("result").innerHTML = getCurrentTab();
-        //alert("URL is NOT using HTTPS");
-    }
-    console.log("URL TESTED");
-}
+            const link = tabs[0].url
+            document.getElementById("url").innerHTML = tabs[0].url
+            if(link.charAt(4) == "s")
+            {
+                document.getElementById("result").innerHTML = "URL is using HTTPS protocol"
+            }
+            else 
+            {
+                alert("URL is NOT using HTTPS Protocol")
+                document.getElementById("result").innerHTML = "URL is NOT using HTTPS protocol"
+            }
 
-async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-  }
+        });
+    //DON'T USE VAR, USE CONST OR LET INSTEAD
+    console.log("URL TESTED")
+})
+
+//$("#create").on("click", function(){ 
+//    alert("CREATING USER")
+
+    //chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
+    //   console.log('Settings saved');
+    //  });
+  
+      // Read it using the storage API
+    //  chrome.storage.sync.get(['foo', 'bar'], function(items) {
+    //    message('Settings retrieved', items);
+    //  });
+
+    // chrome.storage.sync.set({'username': 'value', 'password': 'password'}, function() {
+    //     value = document.getElementById("uname").value
+    //     password = document.getElementById("pword").value
+    // })
+
+
+    // chrome.storage.sync.get(['username'], function(result) {
+    //     alert('Username currently is ' + result.key)
+    // })
+//})
--- testing.js
+++ testing.js
@@ -1,3 +1,17 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Test");
 });
+
+
+$("#create").on("click", function(){
+  chrome.storage.sync.set({username: document.getElementById('uname').value, password: document.getElementById('pword').value }, function() {
+    console.log('Settings saved');
+  });  
+})
+
+
+$("#retrieve").on("click", function(){
+  chrome.storage.sync.get(['username', 'password'], function(items) {
+    console.log('Settings retrieved', items);
+  });
+})
