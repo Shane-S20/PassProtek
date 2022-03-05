cfcf997c7d97aee047cdc23ad3e0332a6ca66665
Shane S <59064868+Shane-S20@users.noreply.github.com>
2022-03-05 00:17:43 -0500

    SPRINT 1 COMPLETE WITH UI
    This build is the completed version of sprint 1 with the implementation of Bootstrap. NO BACK-END FUNCTIONALITY WAS CHANGED.

---
+++ DeleteCredentials.js
@@ -0,0 +1,30 @@
+let db = null;
+
+function open_database() {
+    let OpenRequest = window.indexedDB.open('PassProtek', 1);
+
+    OpenRequest.onsuccess = function(event) {
+        console.log("DELETE DB OPENED");
+        db = event.target.result;
+    }
+}
+
+open_database();
+
+window.onload = function() {
+    document.getElementById("delete").onclick = () => {
+        const transaction = db.transaction("Data","readwrite");
+        const Data = transaction.objectStore("Data");
+        const Requested_Website = document.getElementById("deleted_credentials").value;
+        const search = Data.delete(Requested_Website);
+        console.log("DELETE BUTTON PRESSED")
+
+        search.onsuccess = () => {
+            document.getElementById("deletesuccess").innerHTML = '<br><div class="alert alert-success"style="width: 280px;text-align: center; margin: auto"><strong> Credentials Successfully Deleted </strong></div>'
+            console.log("DATA DELETED SUCCESSFULLY")
+        }
+        search.onerror = () => {
+            console.log("THERE WAS AN ERROR DELETING DATA")
+        }
+    }    
+};
---
+++ TestDB.js
@@ -0,0 +1,116 @@
+const encryptWithAES = (text, passphrase) => {
+    return CryptoJS.AES.encrypt(text, passphrase).toString();
+  };
+
+const decryptWithAES = (ciphertext, passphrase) => {
+    const bytes = CryptoJS.AES.decrypt(ciphertext, passphrase);
+    const originalText = bytes.toString(CryptoJS.enc.Utf8);
+    return originalText;
+};
+
+
+let db = null;
+
+function create_database() {
+    let OpenRequest = window.indexedDB.open('PassProtek', 1);
+
+    OpenRequest.onerror = function (event) {
+        console.log("Problem opening DB.");
+    }
+
+    OpenRequest.onupgradeneeded = function (event) {
+        db = event.target.result;
+
+        let objectStore = db.createObjectStore('Data', {
+            keyPath: 'Website_Name'
+        });
+
+        objectStore.transaction.oncomplete = function (event) {
+            console.log("ObjectStore Created.");
+        }
+    }
+
+    OpenRequest.onsuccess = function(event) {
+        db = event.target.result;
+        console.log("DB OPENED");
+    }
+}
+
+let db2 = null;
+open_user_database();
+
+
+function open_user_database() {
+    let OpenRequest = window.indexedDB.open('PassProtekUsers', 1);
+
+    // OpenRequest.onerror = function (event) {
+    //     console.log("Problem opening DB.");
+    // }
+
+    // OpenRequest.onupgradeneeded = function (event) {
+    //     db2 = event.target.result;
+
+    //     let objectStore = db2.createObjectStore('Users', {
+    //         keyPath: 'Username'
+    //     });
+
+    //     objectStore.transaction.oncomplete = function (event) {
+    //         console.log("Users ObjectStore Created.");
+    //     }
+    // }
+
+    OpenRequest.onsuccess = function(event) {
+        db2 = event.target.result;
+        console.log("MASTER LOGIN DB OPENED");
+    }
+}
+
+// let pass;
+// const transaction2 = db2.transaction("Users","readonly");
+// const User = transaction2.objectStore("Users");
+// const user_search = User.get("User1");
+//     user_search.onsuccess = () => {
+//         //console.log(user_search.result.Username);
+//         //console.log(user_search.result.Password);
+//         pass = user_search.result.Password;
+//         console.log("USER'S PASSWORD")
+//         console.log(pass)
+//     }
+
+
+window.onload = function() {
+    document.getElementById("store").onclick = () => {
+        //var transaction = db.transaction(["PassProtek"], "readwrite");
+        //const objectStore = insert_transaction.objectStore("testCases");
+        const siteName = document.getElementById("siteName").value;
+        const loginUname = btoa(document.getElementById("loginUname").value);
+        const loginPword = btoa(document.getElementById("loginPword").value);
+        const loginURL = btoa(document.getElementById("loginURL").value);
+        const transaction = db.transaction("Data","readwrite")
+        const profiles = transaction.objectStore("Data")
+
+        const randomString = Math.random().toString(36).substr(2,20);
+        console.log(randomString)
+
+        //'C1E13074578EB93751086553FF7DE49E76FCCBB13F80EC3DDD06E2E7DE1A3198'
+        const encryptPassword = encryptWithAES(loginPword, btoa(randomString));
+        const encryptUname = encryptWithAES(loginUname, btoa(randomString))
+        const encryptURL = encryptWithAES(loginURL, btoa(randomString))
+        
+        console.log("OUTSIDE FUNCTION")
+        console.log(encryptURL)
+        const profile = {Website_Name: siteName,Login_Username: encryptUname,Login_Password: encryptPassword,Login_URL: encryptURL,Encoded_Key: btoa(randomString)}
+        const request = profiles.add(profile)
+        
+        console.log("SUBMIT BUTTON PRESSED")
+        request.onsuccess = () => {
+            document.getElementById("storesuccess").innerHTML = '<br><div class="alert alert-success"style="width: 280px;text-align: center; margin: auto"><strong> Credentials Stored Successfully! </strong></div>'
+            console.log("DATA STORE SUCCESSFULLY")
+        }
+        request.onerror = () => {
+            console.log("THERE WAS AN ERROR STORING DATA")
+        }
+    }
+};
+
+create_database();
