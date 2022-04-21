19949fec20318a0dc59ab7a7b71390628d0c5131
Shane S <59064868+Shane-S20@users.noreply.github.com>
2022-04-21 19:10:23 -0400

    Pass Protek 4/21
    Build as of 4/21

---
+++ layout.js
@@ -0,0 +1,81 @@
+let db = null;
+function create_database() {
+    let OpenRequest = window.indexedDB.open('PassProtek', 1);
+
+    OpenRequest.onerror = function (event) {
+        console.log("Problem opening DB.");
+    }
+    OpenRequest.onupgradeneeded = function (event) {
+        db = event.target.result;
+
+        let objectStore = db.createObjectStore('Data', {
+            keyPath: 'Website_Name'
+        });
+
+        objectStore.transaction.oncomplete = function (event) {
+            console.log("ObjectStore Created.");
+        }
+    }
+    OpenRequest.onsuccess = function(event) {
+        db = event.target.result;
+        console.log("DB OPENED");
+    }
+}
+
+
+
+let db3 = null;
+function create_network_database() {
+    let OpenRequest = window.indexedDB.open('PassProtekNetworks', 1);
+    OpenRequest.onerror = function (event) {
+        console.log("Problem opening DB.");
+    }
+    OpenRequest.onupgradeneeded = function (event) {
+        db3 = event.target.result;
+
+        let objectStore = db3.createObjectStore('IPaddr', {
+            keyPath: 'IPaddress'
+        });
+        objectStore.transaction.oncomplete = function (event) {
+            console.log("Network ObjectStore Created.");
+        }
+    }
+    OpenRequest.onsuccess = function(event) {
+        db3 = event.target.result;
+        console.log("Network DB Opened");
+    }
+}
+
+let IPdata = "0";
+//retrieve ip
+$.fn.delegateJSONResult = function(ip){
+    IPdata = ip
+} 
+
+
+
+window.onload = function() {
+    create_network_database();
+    create_database();
+    $.getJSON("https://api.ipify.org?format=json", function (data) {
+        $.fn.delegateJSONResult(data.ip);
+    });
+
+    document.getElementById("get").onclick = () => {
+        const transaction = db3.transaction("IPaddr", "readonly");
+        const Data = transaction.objectStore("IPaddr");
+     
+        console.log(IPdata)
+        const search = Data.openCursor(IPdata);
+
+        search.onsuccess = function(event) {
+            console.log("Search successful");
+            var cursor = event.target.result;
+            if(cursor){console.log("security passed, user may proceed")} else {alert("YOU ARE NOT ON A TRUSTED NETWORK")}
+        };
+
+        search.onerror = () => {
+            console.log("Search failed");
+        };
+    }
+}
